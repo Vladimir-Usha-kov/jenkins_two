7487ab39fe34480be158c28b6c339a244922260d
Vladimir Ushakov <ushakov.vladimir98@mail.ru>
2025-01-22 21:24:10 +0400

    add security and add dotenv

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
+import os
+
 import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
-
+from dotenv import load_dotenv
 from utils import attach
 
 
@@ -14,7 +16,9 @@ def pytest_addoption(parser):
         '--browser_version',
         default='100.0'
     )
-
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
@@ -31,8 +35,11 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
